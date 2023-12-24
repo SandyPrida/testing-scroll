2b580b881eec80e0f0be91be70d40929e3152f9c
SandyPrida <sandy752005@gmail.com>
2023-12-24 20:05:55 +0700

    scroll time

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function App() {
     useEffect(() => {
         const interval = setInterval(() => {
             setIsDragging(true);
-        }, 1500);
+        }, 1000);
 
         // Membersihkan interval saat komponen tidak lagi ter-render
         return () => clearInterval(interval);
